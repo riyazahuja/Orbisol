ff54bcd7ed54d6d03dffbe41d691ce5ab1648cb3
riyazahuja <riyaza@andrew.cmu.edu>
2023-09-15 21:28:37 -0400

    listify data

--- src/datasets/compile.py
+++ src/datasets/compile.py
@@ -5,20 +5,20 @@ import json
 
 
 def generateDict(files):
-    result = {}
+    result = []
     for file in files:
         with open(f'TLE/{file}.txt') as f:
             lines = f.readlines()
-        raw_data = {}
+
         i=0
         while i in range(len(lines)):
             s = f"{lines[i]}{lines[i+1]}{lines[i+2]}"
-            raw_data[lines[i]] = s
+            result.append(s)
             i+=3
         
 
-        result[file] = raw_data
-    print(result)
+       
+    #print(result)
     return result
 
 
