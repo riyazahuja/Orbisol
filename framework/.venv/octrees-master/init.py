7b7cfb2d58a4375e2e890a6470c79ebf541b1537
ArturoParas <aparas@andrew.cmu.edu>
2023-09-16 04:07:09 -0400

    feature: check_in_bounds returns bool function

--- framework/.venv/octrees-master/init.py
+++ framework/.venv/octrees-master/init.py
@@ -40,16 +40,12 @@ def main():
   
   
   current_directory = os.getcwd()
-  print(f"The current working directory is: {current_directory}")
   final_path = 'src/datasets/final.json'
   with open(final_path, 'r') as f:
     celestial_data = json.load(f)
 
   # Generate satellite objects from TLE data and store them in a list (for now).
   ts = Satellite.get_timescale()
-  # satellites = []
-  # ts = Satellite.get_timescale()
-  # now = ts.now()
   satellites = []
   for name, lines in celestial_data.items():
     line1 = lines['line1']
@@ -57,25 +53,14 @@ def main():
     TLE = (line1, line2)
     satellite = Satellite(name, TLE)
     satellites.append(satellite)
-  # for name, lines in celestial_data.items():
-  #   line1 = lines['line1']
-  #   line2 = lines['line2']
-  #   TLE = (line1, line2)
-  #   earth_satellite = EarthSatellite(line1, line2, name, ts)
-  #   print(earth_satellite.at(now).position.km)
-  #   path = lambda t: earth_satellite.at(t).position.km
-  #   satellite = Satellite(name, TLE, path)
-  #   satellites.append(satellite)
-  print(satellites)
 
   bounds = ((-100,100),(-100,100),(-100,100))
   scene = Octree(bounds)
 
   initialTime = ts.now()
   for sat in satellites:
-    # print(f'{sat.name} : {sat.get_pos(initialTime)}')
     initialPos = sat.get_pos(initialTime)
-    p = (initialPos[0]/1000, initialPos[1]/1000, initialPos[2]/1000)
+    p = (initialPos[0], initialPos[1], initialPos[2])
     
     try:
       scene.insert(p,sat)
