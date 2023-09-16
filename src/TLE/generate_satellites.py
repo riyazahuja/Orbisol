f40c5530f05cce984f10391f227e8c1f985a4ccd
ArturoParas <aparas@andrew.cmu.edu>
2023-09-15 23:47:31 -0400

    feature: get_paths is now generate_satellites

--- src/TLE/generate_satellites.py
+++ src/TLE/generate_satellites.py
@@ -3,23 +3,35 @@ import json
 from skyfield.api import load, EarthSatellite
 from functools import partial
 
+class Satellite:
+  ts = load.timescale()
+
+  def __init__(self, name, TLE, path):
+    self.name = name
+    self.type = type
+    self.TLE = TLE
+    self.path = path
+
+  def get_timescale(self):
+    return ts
+
+  def get_pos(self, t):
+    return self.path(t)
+  
 # Get the json data containing celestial objects orbiting earth in TLE format.
 data_path = '../datasets'
 final_path = data_path + '/final.json'
 with open(final_path, 'r') as f:
   celestial_data = json.load(f)
 
-# Define a timescale.
-# TODO: decide whether a timescale should be passed as an argument to this
-# file, or this file creates a timescale and passes it to our struct thingy.
-ts = load.timescale() # Don't even know what timescale is tbh.
-
 # TODO: Create an octree of 
-paths = []
+ts = Satellite.ts
+satellites = []
 for name, lines in celestial_data.items():
   line1 = lines['line1']
   line2 = lines['line2']
-  print(name)
+  TLE = (line1, line2)
   satellite = EarthSatellite(line1, line2, name, ts)
   path = partial(satellite.at)
-  paths.append(path)
+  satellite = Satellite(name, TLE, path)
+  satellites.append(satellite)
